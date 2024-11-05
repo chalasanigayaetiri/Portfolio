507af153cd258c55926ddd5646ec9501e6217693
Gayaetiri Chalasani <gayaetirichalasani@Gayaetiris-MacBook-Pro.local>
2024-11-04 20:28:14 -0600

    Initial commit with portfolio project files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission handling (you can expand this later)
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Thank you for your message!");
+});
